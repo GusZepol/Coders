61dbe3b788109183301abc17d806eea95f6bc389
Gus <biersackandy1990@gmail.com>
2021-10-25 01:48:35 -0500

    se modifica la pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
-        <div>
-            Login Gus
+        <div className='flex flex-col w-full justify-center items-center'>
+            <h2 className='my-3 text-center text-3xl font-extrabold text-gray-900'>
+                Inicia Sesión con tu cuenta
+            </h2>
+            <form className='mt-8 max-w-md'>
+                <div>
+                    <input 
+                    className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                    type='email'
+                    placeholder='sensazione@gmail.com'
+                    required
+                    />                
+                    <input
+                    className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                    type='password'
+                    required
+                    />
+                </div> 
+                <div className='flex justify-between'>
+                    <div>
+                        <label htmlFor='recuerdame'>
+                        <input type='checkbox' name='recuerdame' />
+                        Recuérdame
+                        </label>
+                    </div>
+                    <div>
+                        <Link to='/'>¿Olvidaste tu contraseña?</Link>
+                    </div>
+                </div>
+                <div>
+                    <Link to='/admin'> 
+                    <button type='submit'>Iniciar Sesion</button>
+                    </Link>
+                </div>
+                <div>O</div>
+                <div>
+                    <button>Continúa con Google</button>
+                </div>
+            </form>
         </div>
     )
 }
